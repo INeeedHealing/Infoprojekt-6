6212d96dc7418315ff1d839e94c8974336bd59ef
Robin Sapakota <Robin Sapakota@192.168.178.62>
2018-05-13 20:15:32 +0200

    InfoProjekt Punkte mit Koordinaten+Klein/Groß Ausgabe

--- src/kleinster/kreis/KleinsterKreis.java
+++ src/kleinster/kreis/KleinsterKreis.java
@@ -1,4 +1,3 @@
-
 package kleinster.kreis;
 
 public class KleinsterKreis {
@@ -6,19 +5,50 @@ public class KleinsterKreis {
     public static void main(String[] args) {
         int anzahlP = 10;
         Tuple[] points = new Tuple[anzahlP];
-        
+
         for (int i = 0; i < anzahlP; i++) {
-            points[i] = new Tuple((int)(Math.random()*100),(int)(Math.random()*100));
+            points[i] = new Tuple((int) (Math.random() * 100), (int) (Math.random() * 100));
+            System.out.println(points[i].x + "," + points[i].y);
         }
-        System.out.println(points[1].x);
         int greatestX = 0;
         int smallestX = 100;
-        for (int i = 0; i < anzahlP; i++) {
-            int b = points[i].x;
-            //f(points[i].x < smallestX)   
+        int greatestY= 0;
+        int smallestY=100;
                 
+        for (int i = 0; i < anzahlP; i++) {
+
+            if (points[i].x < smallestX) {
+                smallestX = points[i].x;
+            }
+
         }
         
-    }
+        for (int i = 0; i < anzahlP; i++) {
+
+            if (points[i].x > greatestX) {
+                greatestX = points[i].x;
+            }
+
+        }
+ 
+        
+        for (int i = 0; i < anzahlP; i++) {
+
+            if (points[i].y < smallestY) {
+                smallestY = points[i].y;
+            }
+
+        }
+ 
+        for (int i = 0; i < anzahlP; i++) {
+
+            if (points[i].y > greatestY) {
+                greatestY= points[i].y;
+            }
+
+        }
+        System.out.println("\n"+smallestX+","+smallestY+"\n"+greatestX+","+greatestY);
     
+    }
+
 }
